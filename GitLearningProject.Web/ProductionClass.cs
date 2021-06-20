32666251b2a3562a3967d4082a3cf74033094486
Shreesh Raj <shreeshraj@outlook.com>
2021-06-20 16:23:07 +0530

    Added one class

---
+++ GitLearningProject.Web/ProductionClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GitLearningProject.Web
+{
+    public class ProductionClass
+    {
+
+        public string GetName()
+        {
+            return "heej";
+        }
+    }
+}
